ea19e5bb1eb8f1ade1b759eb90bca378a4b69c20
pelolsen <pelolsen@hotmail.com>
2023-09-20 19:06:32 +0200

    Fixed

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import HomeScreen from './components/HomeScreen';
+import ProfileScreen from './components/ProfileScreen';
+import PlatformScreen from './components/PlatformScreen';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import {NavigationContainer} from "@react-navigation/native";
+
+//Her oprettes en instans af drawernavigator
+const Drawer = createDrawerNavigator();
+
+//I return()  oprettes først en NavigationContainer, som wrapper en Drawer.Navigator
+//Drawer.Navigator wrapper tre screens, som får defineret rutenavne og referencer til de komponenter
+//som skal fremvises i de enkelte screens
+//Komponentern importeres fra "components" mappen.
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name='Platform' component={PlatformScreen}/>
+        <Drawer.Screen name='Profile' component={ProfileScreen}/>
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
